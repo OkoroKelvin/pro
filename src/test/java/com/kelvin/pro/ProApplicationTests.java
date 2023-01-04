04099fc13e72b980352390a95102616ffef014a5
OKORO KELVIN <okorokelvinemoakpooke@yahoo.com>
2023-01-04 10:54:35 +0100

    setup

---
+++ src/test/java/com/kelvin/pro/ProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvin.pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
